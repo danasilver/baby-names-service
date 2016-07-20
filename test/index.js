73f74dc3c41703706a5b9dff24fc700ec7940105
Dana Silver <dsilver1221@gmail.com>
2016-07-19 22:15:42 -0700

    Move expected value to its test.

--- test/index.js
+++ test/index.js
@@ -4,15 +4,14 @@ const path = require('path');
 const request = require('supertest');
 const app = require('..');
 
-const expected = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'dana_f.json'), 'utf-8'));
-
 request(app)
   .get('/Dana/F')
   .expect('Content-Type', /json/)
   .expect(200)
   .end((err, res) => {
     if (err) throw err;
-    assert.deepEqual(res.body, expected);
+    const expected = fs.readFileSync(path.resolve(__dirname, 'dana_f.json'), 'utf-8');
+    assert.deepEqual(res.body, JSON.parse(expected));
   });
 
 request(app)
